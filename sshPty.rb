e4277fc3e4d7ca4e3a0c28f5e5d856863154de67
Firm Lee <liulijen@gmail.com>
2010-03-15 14:46:50 +0800

    Modified sshPty to project

--- sshPty.rb
+++ sshPty.rb
@@ -2,12 +2,16 @@ require 'pty'
 require 'expect'
 
 $expect_verbose = true
-PTY.spawn("ssh @") do |reader, writer, pid|
+PTY.spawn("ssh orthrus@#{ARGV[0]}") do |reader, writer, pid|
   reader.expect(/^.*password:/)
-  writer.puts("\r")
-  reader.expect(/^.*li88-83:~\$/)
-  writer.puts("ls -l\r")
-  reader.expect(/^total.*/)
+  writer.puts("cisco123\r")
+  reader.expect(/^Orthrus-sip\#/)
+  writer.puts("dbg\r")
+  reader.expect('>')
+  writer.puts("switch MXP\r")
+  reader.expect('>')
+  writer.puts("sa pcu show-server\r")
+  reader.expect(/^exit.*/)
   answer = reader.gets
   puts "\n\nAnswer = #{answer}"
 end
