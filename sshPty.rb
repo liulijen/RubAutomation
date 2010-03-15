54536a4b6840f03ce77f4c7f0221f9263bc78683
Firm Lee <liulijen@gmail.com>
2010-03-15 20:03:39 +0800

    Modelize the loggin part

--- sshPty.rb
+++ sshPty.rb
@@ -15,3 +15,17 @@ PTY.spawn("ssh orthrus@#{ARGV[0]}") do |reader, writer, pid|
   answer = reader.gets
   puts "\n\nAnswer = #{answer}"
 end
+
+def spawnSSH
+  reader.expect(/^.*password:/)
+  writer.puts("cisco123\r")
+  reader.expect(/^Orthrus-sip\#/)
+  writer.puts("dbg\r")
+  reader.expect('>')
+  writer.puts("switch MXP\r")
+  reader.expect('>')
+  writer.puts("sa pcu show-server\r")
+  reader.expect(/^exit.*/)
+  answer = reader.gets
+  puts "\n\nAnswer = #{answer}"
+end
