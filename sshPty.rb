ecae45c88b2f9f05a1b8b7d851da6e0cc14d8cb8
Firm Lee <liulijen@gmail.com>
2010-03-18 20:26:48 +0800

    03182026

--- sshPty.rb
+++ sshPty.rb
@@ -2,18 +2,19 @@ require 'pty'
 require 'expect'
 
 $expect_verbose = true
-
+@account = 'orthrus'
+@password = 'cisco123'
 #def spawnSSH
-  reader, writer, pid=PTY.spawn("ssh orthrus@#{ARGV[0]}") 
+  reader, writer, pid=PTY.spawn("ssh #{@account}@#{ARGV[0]}") 
   reader.expect(/^.*password:/)
-  writer.puts("cisco123\r")
+  writer.puts("#{@password}\r")
   reader.expect(/^Orthrus-sip\#/)
   writer.puts("dbg\r")
   reader.expect('>')
   writer.puts("switch MXP\r")
   reader.expect('MXP>MXP>')
-  writer.puts("sa pcu show-server\r\r")
-  reader.expect('MXP>')
+  writer.puts("sa pcu show-server\r")
+  reader.expect(/.*MXP>$/)
   #answer = reader.gets
   #puts "\n\nAnswer = #{answer}"
   
