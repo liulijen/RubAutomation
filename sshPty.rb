9427a7e9a8867b0e176e7836e61d819ee77e8cd5
Firm Lee <liulijen@gmail.com>
2010-03-19 09:48:46 +0800

    Modified sshPty to show version server and LSC key

--- sshPty.rb
+++ sshPty.rb
@@ -4,7 +4,7 @@ require 'expect'
 $expect_verbose = true
 @account = 'orthrus'
 @password = 'cisco123'
-#def spawnSSH
+def showServer 
   reader, writer, pid=PTY.spawn("ssh #{@account}@#{ARGV[0]}") 
   reader.expect(/^.*password:/)
   writer.puts("#{@password}\r")
@@ -12,13 +12,56 @@ $expect_verbose = true
   writer.puts("dbg\r")
   reader.expect('>')
   writer.puts("switch MXP\r")
-  reader.expect('MXP>MXP>')
   writer.puts("sa pcu show-server\r")
-  reader.expect(/.*MXP>$/)
-  #answer = reader.gets
-  #puts "\n\nAnswer = #{answer}"
-  
-#end
-#spawnSSH
 
-	
+  reader.expect('2010')
+  writer.puts('\r\r')
+  5.times{
+    answer = reader.gets
+    puts "#{answer}"
+  }
+end
+def showSWversion 
+  reader, writer, pid=PTY.spawn("ssh #{@account}@#{ARGV[0]}") 
+  reader.expect(/^.*password:/)
+  writer.puts("#{@password}\r")
+  reader.expect(/^Orthrus-sip\#/)
+  writer.puts("dbg\r")
+  reader.expect('>')
+  writer.puts("switch SHELL\r")
+  reader.expect('~ #')
+  writer.puts("cat /etc/versions\r")
+  
+  7.times{
+    answer = reader.gets
+    puts "#{answer}"
+  }
+end
+def showLSCkey 
+  reader, writer, pid=PTY.spawn("ssh #{@account}@#{ARGV[0]}") 
+  reader.expect(/^.*password:/)
+  writer.puts("#{@password}\r")
+  reader.expect(/^Orthrus-sip\#/)
+  writer.puts("dbg\r")
+  reader.expect('>')
+  writer.puts("switch SHELL\r")
+  reader.expect('~ #')
+  writer.puts("ls /var/voice_conf/sec1/lsc0\r")
+  writer.puts("ls /var/voice_conf/sec2/lsc0\r")
+  6.times{
+    answer = reader.gets
+    puts "#{answer}"
+  }
+
+ 
+end
+case ARGV[1]
+  when '1' then
+    showServer
+  when '2' then
+    showSWversion
+  when '3' then
+    showLSCkey
+  else
+    puts "No Input" 
+end		 
