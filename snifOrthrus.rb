5ddd5e523655c25ab955d8ecd8b7277c270f1a64
Firm Lee <liulijen@gmail.com>
2010-04-30 08:13:36 +0800

    snifOrthrus update V0.2

--- snifOrthrus.rb
+++ snifOrthrus.rb
@@ -26,15 +26,24 @@ end
 def cTrivial(ori)
    return "\33[2;47;40m#{ori}\33[0m"
 end
+def cRed(ori)
+   return "\33[1;31;40m#{ori}\33[0m"
+end
 def sysTime
    t=Time.new
    return cTrivial(t.strftime("(#{ARGV[0][-5..-1]})%H:%M:%S"))
+end
+def putLine(text)
+	print "\n"+text
+end
+def printLine(text)
+    print text
 end
 trap("INT") { 
 	puts "#{cTrivial("User stop analyzing!")}"
     Process.exit
 }
-  puts "Capturing.."
+  printLine("Capturing..")
   cRTP=0; #RTP show interval
   tCDP=0; #CDP timer
   loop{
@@ -44,40 +53,50 @@ trap("INT") {
     #puts answer
 	case answer
     when /NOTIFY sip.* \(text\/plain\)/
-       puts "#{sysTime} CUCM send #{cEvent("Reset or Restart")}"
+       putLine("#{sysTime} CUCM send #{cEvent("Reset or Restart")}")
     when /CDP Device ID: (\S+)/
-       puts "#{sysTime}\t #{cTrivial("CDP: device ID is #{$1}")}"
+       putLine("#{sysTime}\t #{cTrivial("CDP: device ID is #{$1}")}")
+    when /ARP Who has (\S+)?/
+       putLine("#{sysTime}\t #{cTrivial("ARP: who? #{$1}")}")
+    when /ARP (\S+) is at (\S+)/
+       printLine(cTrivial(".. is ")+$2)
     when /Request: INVITE sip/
-       puts "#{sysTime}\t INVITE a call"
+       putLine("#{sysTime}\t INVITE a call")
     when /Request: BYE sip/
-       puts "#{sysTime}\t BYE a call"
+       putLine("#{sysTime}\t BYE a call")
     when /DHCP Request/
-       puts "#{sysTime}\t #{cTrivial("DHCP Request")}"
+       putLine("#{sysTime}\t #{cTrivial("DHCP Request")}")
+    when /DHCP ACK/
+       putLine("#{sysTime}\t #{cTrivial("DHCP ACK")}")
     when /Gratuitous ARP for (.*) /
        #puts "#{sysTime}\t #{cEvent("GARP")} for #{$1}"
-       puts "#{sysTime}\t #{cTrivial("GARP for #{$1}")}"
+       putLine("#{sysTime}\t #{cTrivial("GARP for #{$1}")}")
     when /-> (\S+) DNS Standard query A (\S+)/
-       puts "#{sysTime}\t #{cEvent("DNS")} query for #{$2} from "+$1
+       putLine("#{sysTime}\t #{cEvent("DNS")}  query for #{$2} from "+$1+"")
+    when /DNS Standard query response A (\S+)/
+       printLine(cEvent(".. response "+$1))
     when /-> (\S+) TFTP Read Request, File: (\S+)\\/
-       puts "#{sysTime}\t #{cEvent("TFTP")} request #{cFileName("#{$2}")} from "+$1
+       putLine("#{sysTime}\t #{cEvent("TFTP")} request #{cFileName("#{$2}")} from "+$1)
     when /PT=ITU-T G.711/
        if cRTP>500
-       	  puts "#{sysTime}\t #{cTrivial("G.711 RTP...sending")}"
+       	  putLine("#{sysTime}\t #{cTrivial("G.711 RTP...sending")}")
           cRTP=0
        else
           cRTP+=1
 	   end
     when /PT=ITU-T G.729/
        if cRTP>500
-       	  puts "#{sysTime}\t #{cTrivial("G.729 RTP...sending")}"
+       	  putLine("#{sysTime}\t #{cTrivial("G.729 RTP...sending")}")
           cRTP=0
        else
           cRTP+=1
 	   end
     when /TFTP Data Packet.*\(last\)/
-       puts "#{sysTime}\t file obtained"
+       printLine(cEvent(".. obtained"))
     when /TFTP Error Code.*Could not open/
-       puts "#{sysTime}\t #{cEvent("file get failed!")}"
+       printLine(cRed(".. failed!"))
+    when /HTTP GET \/(\S+)/
+	   putLine("#{sysTime}\t HTTP #{cTrivial("sent #{$1}")}")
     end
   }
 
